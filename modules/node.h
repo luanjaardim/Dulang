2171714d8871255297dbc1e770320b8239131382
luanjaardim <luang.ajardim@gmail.com>
2024-03-30 10:09:23 -0300

    Node lib for building ASTs

--- modules/node.h
+++ modules/node.h
@@ -1,52 +1,83 @@
 #ifndef NODE_LIB_DEF
 #define NODE_LIB_DEF
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
+#include "utils.h"
 
-typedef struct Node Node;
+template <typename T>
+struct Node {
+private:
+  bool freed = false;
+  void (* delete_data)(T data) = NULL;
+  T data;
+  vector<Node<T>*> neighbors;
 
-Node *node_create(void *value, size_t data_size);
-Node *node_create_null(size_t data_size);
-void node_delete(Node *n, void (*delete_data)(void *));
-unsigned node_delete_recursive(Node *n, void (*delete_data)(void *));
-void node_get_value(Node *n, void *to_ret, size_t data_size);
-void node_set_value(Node *n, void *to_ret, void *to_add, size_t data_size);
-void node_set_link(Node *n, Node *n2);
-void node_set_link_at(Node *n, Node *n2, unsigned position);
-void node_set_double_link(Node *n, Node *n2);
-void node_set_double_link_at(Node *n, Node *n2, unsigned position_n, unsigned position_n2);
-Node *node_get_neighbour(Node *n, unsigned neighbour_num);
-void node_change_neighbour_position(Node *n, unsigned first_position, unsigned second_position);
-unsigned node_remove_link(Node *node, Node *node_to_remove);
-Node *node_remove_link_at(Node *n, unsigned position);
-void node_swap_neighbours(Node *n1, Node *n2, unsigned first_position, unsigned second_position);
-unsigned node_get_num_neighbours(Node *n);
-void *node_data_pnt(Node *n);
-
-#ifndef INIT_NODE_TYPE
-#define INIT_NODE_TYPE(name, type) \
-  size_t name##_data_size = sizeof(type); \
-  Node *name##_node_create(type value) { \
-    return node_create((void *) &value, name##_data_size); \
-  } \
-  Node *name##_node_create_null() { \
-    return node_create_null(name##_data_size); \
-  } \
-  type name##_node_get_value(Node *n) { \
-    type val; \
-    node_get_value(n, (void *) &val, name##_data_size); \
-    return val; \
-  } \
-  type name##_node_set_value(Node *n, type value) { \
-    type val; \
-    node_set_value(n, (void *) &val, (void *) &value, name##_data_size); \
-    return val; \
+public:
+  Node(T data) : data(data) {}
+  Node(T data, void (* delete_data)(T data)) : data(data) {
+    this->delete_data = delete_data;
   }
-#endif /* INIT_NODE_TYPE */
+
+  ~Node() {
+    freed = true;
+    if(delete_data != NULL) {
+      delete_data(data);
+    }
+    for (auto neighbor : neighbors) {
+      if(neighbor->freed)
+        continue;
+      delete neighbor;
+    }
+  }
+  T get_data() { return data; }
+  void set_data(T data) { 
+    if(delete_data != NULL)
+      delete_data(this->data);
+    this->data = data; 
+  }
+  Node<T>* get_neighbor(int index) { return neighbors[index]; }
+  void push_neighbor_with_data(T data) {
+    neighbors.push_back(new Node(data, delete_data));
+  }
+  void push_neighbor(Node<T>* neighbor) {
+    neighbors.push_back(neighbor);
+  }
+  void simple_link(Node<T>* neighbor) {
+    neighbors.push_back(neighbor);
+  }
+  void simple_link_at(Node<T>* neighbor, int index) {
+    neighbors.insert(neighbors.begin() + index, neighbor);
+  }
+  void link(Node<T>* neighbor) {
+    push_neighbor(neighbor);
+    neighbor->push_neighbor(this);
+  }
+  //index is the index of the neighbor
+  //neighbor_index is the index of this node to the neighbor
+  void link_at(Node<T>* neighbor, int index, int neighbor_index) {
+    neighbors.insert(neighbors.begin() + index, neighbor);
+    neighbor->neighbors.insert(neighbor->neighbors.begin() + neighbor_index, this);
+  }
+
+  void unlink(Node<T>* neighbor) {
+    for (int i = 0; i < neighbors.size(); i++) {
+      if (neighbors[i] == neighbor) {
+        neighbors.erase(neighbors.begin() + i);
+        break;
+      }
+    }
+    for (int i = 0; i < neighbor->neighbors.size(); i++) {
+      if (neighbor->neighbors[i] == this) {
+        neighbor->neighbors.erase(neighbor->neighbors.begin() + i);
+        break;
+      }
+    }
+  }
+  // void print_nodes() {
+  //   cout << "Node: " << data << endl;
+  //   for (auto neighbor : neighbors) {
+  //     cout << "\tNeighbor: " << neighbor->data << endl;
+  //   }
+  // }
+};
 
 #endif /* NODE_LIB_DEF */
